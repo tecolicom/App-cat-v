37064d301601fce062a871a379673a3af6272bd8
Kazumasa Utashiro <kaz@utashiro.com>
2024-03-22 19:03:17 +0900

    minimum update

--- lib/App/cat_v.pm
+++ lib/App/cat_v.pm
@@ -1,39 +1,38 @@
-package App::cat::v;
-use 5.008001;
+package App::cat_v;
+use 5.014;
 use strict;
 use warnings;
 
 our $VERSION = "0.01";
 
-
-
 1;
+
 __END__
 
 =encoding utf-8
 
 =head1 NAME
 
-App::cat::v - It's new $module
+App::cat_v - cat-v command implementation
 
 =head1 SYNOPSIS
 
-    use App::cat::v;
+    use App::cat_v;
 
 =head1 DESCRIPTION
 
-App::cat::v is ...
+App::cat_v is the body of the C<script/cat-v> command.
 
 =head1 LICENSE
 
-Copyright (C) Kazumasa Utashiro.
+Copyright ©︎ 2024 Kazumasa Utashiro.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+Kazumasa Utashiro
 
 =cut
 
