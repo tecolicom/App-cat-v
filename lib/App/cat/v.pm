17b000f810157fe4ba52762c1614253c674a121e
Kazumasa Utashiro <kaz@utashiro.com>
2024-03-28 09:59:15 +0900

    revice total implementation

--- lib/App/cat/v.pm
+++ lib/App/cat/v.pm
@@ -2,12 +2,11 @@ package App::cat::v;
 
 our $VERSION = "0.01";
 
-use 5.014;
+use 5.024;
 use warnings;
 
 use utf8;
 use Encode;
-use charnames ':full';
 use Data::Dumper;
 {
     no warnings 'redefine';
@@ -22,59 +21,75 @@ use Hash::Util qw(lock_keys);
 use Getopt::EX;
 use Text::ANSI::Tabs qw(ansi_expand);
 
-my %control  = (
-    nul  => [ 's', "\000", "\x{2400}" ], # ␀ SYMBOL FOR NULL
-    soh  => [ 's', "\001", "\x{2401}" ], # ␁ SYMBOL FOR START OF HEADING
-    stx  => [ 's', "\002", "\x{2402}" ], # ␂ SYMBOL FOR START OF TEXT
-    etx  => [ 's', "\003", "\x{2403}" ], # ␃ SYMBOL FOR END OF TEXT
-    eot  => [ 's', "\004", "\x{2404}" ], # ␄ SYMBOL FOR END OF TRANSMISSION
-    enq  => [ 's', "\005", "\x{2405}" ], # ␅ SYMBOL FOR ENQUIRY
-    ack  => [ 's', "\006", "\x{2406}" ], # ␆ SYMBOL FOR ACKNOWLEDGE
-    bel  => [ 's', "\007", "\x{2407}" ], # ␇ SYMBOL FOR BELL
-    bs   => [ 's', "\010", "\x{2408}" ], # ␈ SYMBOL FOR BACKSPACE
-    ht   => [ 's', "\011", "\x{2409}" ], # ␉ SYMBOL FOR HORIZONTAL TABULATION
-    nl   => [ '0', "\012", "\x{240A}" ], # ␊ SYMBOL FOR LINE FEED
-    vt   => [ 's', "\013", "\x{240B}" ], # ␋ SYMBOL FOR VERTICAL TABULATION
-    np   => [ 's', "\014", "\x{240C}" ], # ␌ SYMBOL FOR FORM FEED
-    cr   => [ 's', "\015", "\x{240D}" ], # ␍ SYMBOL FOR CARRIAGE RETURN
-    so   => [ 's', "\016", "\x{240E}" ], # ␎ SYMBOL FOR SHIFT OUT
-    si   => [ 's', "\017", "\x{240F}" ], # ␏ SYMBOL FOR SHIFT IN
-    dle  => [ 's', "\020", "\x{2410}" ], # ␐ SYMBOL FOR DATA LINK ESCAPE
-    dc1  => [ 's', "\021", "\x{2411}" ], # ␑ SYMBOL FOR DEVICE CONTROL ONE
-    dc2  => [ 's', "\022", "\x{2412}" ], # ␒ SYMBOL FOR DEVICE CONTROL TWO
-    dc3  => [ 's', "\023", "\x{2413}" ], # ␓ SYMBOL FOR DEVICE CONTROL THREE
-    dc4  => [ 's', "\024", "\x{2414}" ], # ␔ SYMBOL FOR DEVICE CONTROL FOUR
-    nak  => [ 's', "\025", "\x{2415}" ], # ␕ SYMBOL FOR NEGATIVE ACKNOWLEDGE
-    syn  => [ 's', "\026", "\x{2416}" ], # ␖ SYMBOL FOR SYNCHRONOUS IDLE
-    etb  => [ 's', "\027", "\x{2417}" ], # ␗ SYMBOL FOR END OF TRANSMISSION BLOCK
-    can  => [ 's', "\030", "\x{2418}" ], # ␘ SYMBOL FOR CANCEL
-    em   => [ 's', "\031", "\x{2419}" ], # ␙ SYMBOL FOR END OF MEDIUM
-    sub  => [ 's', "\032", "\x{241A}" ], # ␚ SYMBOL FOR SUBSTITUTE
-    esc  => [ '0', "\033", "\x{241B}" ], # ␛ SYMBOL FOR ESCAPE
-    fs   => [ 's', "\034", "\x{241C}" ], # ␜ SYMBOL FOR FILE SEPARATOR
-    gs   => [ 's', "\035", "\x{241D}" ], # ␝ SYMBOL FOR GROUP SEPARATOR
-    rs   => [ 's', "\036", "\x{241E}" ], # ␞ SYMBOL FOR RECORD SEPARATOR
-    us   => [ 's', "\037", "\x{241F}" ], # ␟ SYMBOL FOR UNIT SEPARATOR
-    sp   => [ 's', "\040", "\x{2420}" ], # ␠ SYMBOL FOR SPACE
-    del  => [ 's', "\177", "\x{2421}" ], # ␡ SYMBOL FOR DELETE
-    nbsp => [ 's', "\240", "\x{2423}" ], # ␣ OPEN BOX
+my %control = (
+    nul  => [ 'm', "\000", { s => "\x{2400}",      # ␀ SYMBOL FOR NULL
+			     m => "\x{2205}", } ], # ∅ EMPTY SET
+    soh  => [ 's', "\001", { s => "\x{2401}", } ], # ␁ SYMBOL FOR START OF HEADING
+    stx  => [ 's', "\002", { s => "\x{2402}", } ], # ␂ SYMBOL FOR START OF TEXT
+    etx  => [ 's', "\003", { s => "\x{2403}", } ], # ␃ SYMBOL FOR END OF TEXT
+    eot  => [ 's', "\004", { s => "\x{2404}", } ], # ␄ SYMBOL FOR END OF TRANSMISSION
+    enq  => [ 's', "\005", { s => "\x{2405}", } ], # ␅ SYMBOL FOR ENQUIRY
+    ack  => [ 's', "\006", { s => "\x{2406}", } ], # ␆ SYMBOL FOR ACKNOWLEDGE
+    bel  => [ 's', "\007", { s => "\x{2407}",      # ␇ SYMBOL FOR BELL
+			     m => "\x{237E}", } ], # ⍾ BELL SYMBOL
+    bs   => [ 's', "\010", { s => "\x{2408}", } ], # ␈ SYMBOL FOR BACKSPACE
+    ht   => [ 's', "\011", { s => "\x{2409}", } ], # ␉ SYMBOL FOR HORIZONTAL TABULATION
+    nl   => [ 'm', "\012", { s => "\x{240A}",      # ␊ SYMBOL FOR LINE FEED
+			     m => "\x{23CE}", } ], # ⏎ RETURN SYMBOL
+    vt   => [ 's', "\013", { s => "\x{240B}", } ], # ␋ SYMBOL FOR VERTICAL TABULATION
+    np   => [ 'm', "\014", { s => "\x{240C}",    , # ␌ SYMBOL FOR FORM FEED
+			     m => "\x{2398}", } ], # ⎘ NEXT PAGE
+    cr   => [ 's', "\015", { s => "\x{240D}", } ], # ␍ SYMBOL FOR CARRIAGE RETURN
+    so   => [ 's', "\016", { s => "\x{240E}", } ], # ␎ SYMBOL FOR SHIFT OUT
+    si   => [ 's', "\017", { s => "\x{240F}", } ], # ␏ SYMBOL FOR SHIFT IN
+    dle  => [ 's', "\020", { s => "\x{2410}", } ], # ␐ SYMBOL FOR DATA LINK ESCAPE
+    dc1  => [ 's', "\021", { s => "\x{2411}", } ], # ␑ SYMBOL FOR DEVICE CONTROL ONE
+    dc2  => [ 's', "\022", { s => "\x{2412}", } ], # ␒ SYMBOL FOR DEVICE CONTROL TWO
+    dc3  => [ 's', "\023", { s => "\x{2413}", } ], # ␓ SYMBOL FOR DEVICE CONTROL THREE
+    dc4  => [ 's', "\024", { s => "\x{2414}", } ], # ␔ SYMBOL FOR DEVICE CONTROL FOUR
+    nak  => [ 's', "\025", { s => "\x{2415}", } ], # ␕ SYMBOL FOR NEGATIVE ACKNOWLEDGE
+    syn  => [ 's', "\026", { s => "\x{2416}", } ], # ␖ SYMBOL FOR SYNCHRONOUS IDLE
+    etb  => [ 's', "\027", { s => "\x{2417}", } ], # ␗ SYMBOL FOR END OF TRANSMISSION BLOCK
+    can  => [ 's', "\030", { s => "\x{2418}", } ], # ␘ SYMBOL FOR CANCEL
+    em   => [ 's', "\031", { s => "\x{2419}", } ], # ␙ SYMBOL FOR END OF MEDIUM
+    sub  => [ 's', "\032", { s => "\x{241A}", } ], # ␚ SYMBOL FOR SUBSTITUTE
+    esc  => [ '0', "\033", { s => "\x{241B}", } ], # ␛ SYMBOL FOR ESCAPE
+    fs   => [ 's', "\034", { s => "\x{241C}", } ], # ␜ SYMBOL FOR FILE SEPARATOR
+    gs   => [ 's', "\035", { s => "\x{241D}", } ], # ␝ SYMBOL FOR GROUP SEPARATOR
+    rs   => [ 's', "\036", { s => "\x{241E}", } ], # ␞ SYMBOL FOR RECORD SEPARATOR
+    us   => [ 's', "\037", { s => "\x{241F}", } ], # ␟ SYMBOL FOR UNIT SEPARATOR
+    sp   => [ 'm', "\040", { s => "\x{2420}",      # ␠ SYMBOL FOR SPACE
+			     m => "\x{00B7}", } ], # · MIDDLE DOT
+    del  => [ 's', "\177", { s => "\x{2421}", } ], # ␡ SYMBOL FOR DELETE
+    nbsp => [ 's', "\240", { s => "\x{2423}", } ], # ␣ OPEN BOX
 );
 
-my %flag   = pairmap { $a => $b->[0] }      %control;
-my %symbol = pairmap { $b->[1] => $b->[2] } %control;
-my %code   = pairmap { $a => $b->[1] }      %control;
-lock_keys %flag;
+package #
+Visibility {
+    use v5.24;
+    use warnings;
+    sub default { $_[0]->[0] }
+    sub code    { $_[0]->[1] }
+    sub cmap    { $_[0]->[2] }
+    sub visible {
+	my($c, $type) = @_;
+	$c->cmap->{$type} // $c->cmap->{$c->default || 's'};
+    }
+};
+bless $_, 'Visibility' for values %control;
 
-my $default_tabstyle = 'pin';
-if ($default_tabstyle) {
-    Text::ANSI::Tabs->configure(tabstyle => $default_tabstyle);
+my %code = pairmap { $a => $b->code } %control;
+
+our $DEFAULT_TABSTYLE = 'pin';
+if ($DEFAULT_TABSTYLE) {
+    Text::ANSI::Tabs->configure(tabstyle => $DEFAULT_TABSTYLE);
 }
 
 sub list_tabstyle {
     my %style = %Text::ANSI::Fold::TABSTYLE;
     my $max = max map length, keys %style;
     for my $name (sort keys %style) {
-	my($head, $space) = @{$style{$name}};
+	my($head, $space) = $style{$name}->@*;
 	my $tab = $head . $space x 7;
 	printf "%*s %s\n", $max, $name, $tab x 3;
     }
@@ -85,26 +100,42 @@ use Getopt::EX::Hashed; {
     has visible    => ' c  =s@ ' ;
     has reset      => ' n      ' ;
     has expand     => ' t  !   ' , default => 1 ;
-    has repeat     => '    =s  ' , default => 'nl' ;
+    has repeat     => '    =s  ' , default => 'nl,np' ;
     has debug      => ' d      ' ;
     has tabstop    => ' x  =i  ' , default => 8, min => 1 ;
     has tabhead    => '    =s  ' ;
     has tabspace   => '    =s  ' ;
-    has tabstyle   => ' ts :s  ' , default => $default_tabstyle ;
+    has tabstyle   => ' ts :s  ' , default => $DEFAULT_TABSTYLE ;
     has help       => ' h      ' ;
     has version    => ' v      ' ;
 
     # -n
     has '+reset' => sub {
-	$flag{$_} = '0' for keys %flag;
+	for my $name (keys $_->flags->%*) {
+	    $_->flags->{$name} = '0';
+	}
     };
 
+    has '+repeat' => sub {
+	my($name, $c) = ("$_[0]", $_[1]);
+	if ($c =~ s/^\++//) {
+	    $_->$name .= ",$c";
+	} else {
+	    $_->$name = $c;
+	}
+    };
+
+    # individual char option
     has [ keys %control ] => ':s', action => sub {
-	my($name, $c) = map "$_", @_;
+	my($name, $c) = ("$_[0]", $_[1]);
+	if ($c =~ s/^\+//) {
+	    $_->repeat .= ",$name";
+	}
 	$c = '1' if $c eq '';
-	$c = charnames::string_vianame($c) || die "$c: invalid name\n"
-	    if length($c) > 1;
-	$flag{$name} = $c;
+	if (length($c) > 1 and my $u = charnames::string_vianame($c)) {
+	    $c = $u;
+	}
+	$_->flags->{$name} = $c;
     };
 
     #  -T negate -t
@@ -132,7 +163,7 @@ use Getopt::EX::Hashed; {
 
     has '+visible' => sub {
 	my $param = $_[1];
-	$param =~ s{ \ball\b }{ join('=', keys %flag) }xe;
+	$param =~ s{ \ball\b }{ join('=', keys $_->flags->%*) }xe;
 	push @{$_->visible}, $param;
     };
 
@@ -147,12 +178,17 @@ use Getopt::EX::Hashed; {
 	exit;
     };
 
+    # internal use
+
+    has flags   => default => { pairmap { $a => $b->default } %control };
+    has convert => default => {};
+
 } no Getopt::EX::Hashed;
 
 sub run {
     my $app = shift;
     local @ARGV = splice @_;
-    $app->options->doit;
+    $app->options->setup->doit;
     return 0;
 }
 
@@ -166,37 +202,50 @@ sub options {
     Configure qw(bundling);
     $app->getopt || pod2usage();
 
-    require Getopt::EX::LabeledParam;
     Getopt::EX::LabeledParam
-	->new(HASH => \%flag, NEWLABEL => 0, DEFAULT => 1)
-	->load_params(@{$app->visible});
+	->new(HASH => $app->flags, NEWLABEL => 0, DEFAULT => 1)
+	->load_params($app->visible->@*);
 
+    return $app;
+}
+
+sub setup {
+    my $app = shift;
+    my $symbol = $app->convert;
+    my $flags = $app->flags;
+    for my $name (keys $flags->%*) {
+	my $flag = $flags->{$name} or next;
+	my $char = $control{$name};
+	my $code = $char->code;
+	if ($flag eq 'c') {
+	    $symbol->{$code} = '^' . pack('c',ord($code)+64);
+	}
+	elsif ($flag =~ /^([a-z\d])$/i) {
+	    $symbol->{$code} = $char->visible($flag);
+	}
+	else {
+	    $symbol->{$char->code} = $flag;
+	}
+    }
     return $app;
 }
 
 sub doit {
     my $app = shift;
-    my($control, $symbol) = ('', '');
-    for my $name (keys %flag) {
-	if ($flag{$name} eq 'c') {
-	    $control .= $code{$name};
+    my $convert = $app->convert;
+    my $replace = join '', sort keys $convert->%*;
+    my $repeat_re = do {
+	if (my @c = map { $code{$_} } $app->repeat =~ /\w+/g) {
+	    local $" = "";
+	    qr/[@c]/;
+	} else {
+	    qr/(?!)/;
 	}
-	elsif (my $c = $flag{$name}) {
-	    $symbol .= $code{$name};
-	    if ($flag{$name} =~ /^([^a-z\d\s])$/i) {
-		$symbol{$code{$name}} = $1;
-	    }
-	}
-    }
-    my $repeat = join '', map { $code{$_} } $app->repeat =~ /\w+/g;
-    my $repeat_re = qr/[$repeat]/;
+    };
     while (<>) {
 	$_ = ansi_expand($_) if $app->expand;
-	s{(?=(${repeat_re}?))([$symbol]|(?#bug?)(?!))}{$symbol{$2}$1}g
-	    if $symbol ne '';
-	s{(?=(${repeat_re}?))([$control]|(?#bug?)(?!))}{
-	    '^' . pack('c',ord($2)+64) . $1
-	}ge if $control ne '';
+	s{(?=(${repeat_re}?))([$replace]|(?#bug?)(?!))}{$convert->{$2}$1}g
+	    if $replace ne '';
 	print;
     }
 }
